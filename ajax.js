e1b6021fc5bd09d07f9cdbab6288ba8d0eadad00
badwolf0174 <bbn00bkiller@gmail.com>
2017-06-27 13:38:58 +0200

    Add files via upload

--- ajax.js
+++ ajax.js
@@ -1,19 +1,4 @@
-$( document ).ready(function() {"use strict";
-$( document ).bind( "mobileinit", function() {
-    // Make your jQuery Mobile framework configuration changes here!
 
-    $.mobile.allowCrossDomainPages = true;
-});});
-function getParameterByName(name, url) {
-	"use strict";
-    if (!url) url = window.location.href;
-    name = name.replace(/[\[\]]/g, "\\$&");
-    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
-}
 function InitAJAX()
 {
 	"use strict";
@@ -47,7 +32,6 @@ function InitAJAX()
 
 window.setInterval(function()
 {
-	"use strict";
 // Maak een XHR object
 var xmlHttp = InitAJAX();
 
@@ -55,7 +39,7 @@ var xmlHttp = InitAJAX();
 xmlHttp.onreadystatechange = function ()
 {
     // Is het request al helemaal klaar en OK?
-    if (xmlHttp.readyState === 4 && xmlHttp.status === 200)
+    if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
     {
         // Lees de tekst die is ontvangen
         var result = xmlHttp.responseText;
@@ -63,9 +47,9 @@ xmlHttp.onreadystatechange = function ()
         // Plaats de tekst in de pagina
         document.getElementById("content").innerHTML = result;
     }
-};
-var id = getParameterByName('car_id'); // "lorem"
+}
+
 // Verstuur het request
-xmlHttp.open("GET", "http://78696.ict-lab.nl/InhaalOpdrachtWeb4Mobile/php/car_detail.php?car_id="+id, true);
+xmlHttp.open("GET", "http://78696.ict-lab.nl/InhaalOpdrachtWeb4Mobile/php/data.php", true);
 xmlHttp.send(null);
 },5000);
